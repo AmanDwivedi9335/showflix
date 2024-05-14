f2edb2112e3f77783f9ca803a10f0baf2fe41812
Aman <amandwdi@gmail.com>
2024-05-14 10:55:09 +0530

    Header and banner completed

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
                   <Route path="*" element={<PageNotFound/>}/>
 
             </Routes>
-        {/* <Footer /> */}
+        <Footer />
     </BrowserRouter>
   )
 }
